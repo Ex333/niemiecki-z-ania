e2438a7d2c9dbe99f8baac21b71ae6d1faa078c1
Ex333 <haldas.mateusz@gmail.com>
2026-03-20 13:26:55 +0100

    new templates and a bit of css

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home, materialy, kontakt
 
 
 urlpatterns = [
     path("", home, name="home"),
+    path("materialy/", materialy, name="materialy"),
+    path("kontakt/", kontakt, name="kontakt"),
 ]
 
--- core/views.py
+++ core/views.py
@@ -4,3 +4,10 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, "home.html")
+
+
+def materialy(request):
+    return render(request, "materialy.html")
+
+def kontakt(request):
+    return render(request, "kontakt.html")
