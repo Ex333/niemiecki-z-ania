72c30ef10d1a44c8571c9eb6bdb69f2839d23711
Ex333 <haldas.mateusz@gmail.com>
2026-03-22 12:05:51 +0100

    Thank You, Forms and so on..

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import home, materialy, kontakt
+from core import views
 
 
 urlpatterns = [
     path("", home, name="home"),
     path("materialy/", materialy, name="materialy"),
     path("kontakt/", kontakt, name="kontakt"),
+    path("dziekuje/", views.dziekuje, name="dziekuje"),
 ]
 
--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import HomePage
+from .forms import ContactForm
+from django.shortcuts import redirect
 
 
 def home(request):
@@ -12,4 +14,18 @@ def materialy(request):
     return render(request, "materialy.html")
 
 def kontakt(request):
-    return render(request, "kontakt.html")
+    form = ContactForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        name = form.cleaned_data["name"]
+        email = form.cleaned_data["email"]
+        message = form.cleaned_data["message"]
+
+        print(name, email, message)
+
+        return redirect("dziekuje")
+
+    return render(request, "kontakt.html", {"form": form})
+
+def dziekuje(request):
+    return render(request, "dziekuje.html")
