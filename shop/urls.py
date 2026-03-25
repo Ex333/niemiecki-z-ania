425f8fa3932ae8c6495665bcd5712751a1e74a6c
Ex333 <haldas.mateusz@gmail.com>
2026-03-25 21:27:34 +0100

    upgrade

--- shop/urls.py
+++ shop/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from shop.views import  sklep, product_detail
-
+from .views import sklep, product_detail
 
 urlpatterns = [
     path("", sklep, name="sklep"),
-    path("<slug:slug>/", product_detail, name="sklep_detail"),
+    path("<slug:slug>/", product_detail, name="product_detail"),
 ]
-
