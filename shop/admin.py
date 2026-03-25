8e53bda3cccf0d71776fcea5f5cd3d4387913489
Ex333 <haldas.mateusz@gmail.com>
2026-03-25 20:27:30 +0100

    Admin Fields  added for shop

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Product, ShopCategory
 
-# Register your models here.
+
+@admin.register(ShopCategory)
+class ShopCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'available', 'created_at']
+    list_filter = ['available', 'created_at', 'category']
+    search_fields = ['name', 'description']
+    prepopulated_fields = {'slug': ('name',)}
