3cffadd97eaa75a511ce729aaaf1efaae9846e2c
Ex333 <haldas.mateusz@gmail.com>
2026-03-25 20:19:16 +0100

    Added model Product for shop

--- shop/models.py
+++ shop/models.py
@@ -1,14 +1,36 @@
 from django.db import models
 
 # =========== SHOP MODELS ==========
+class ShopCategory(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        ordering = ['name']  # sortowanie kategorii A-Z
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
+    category = models.ForeignKey(
+        ShopCategory,
+        on_delete=models.CASCADE,
+        related_name="products"
+    )
     name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(unique=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    file = models.FileField(upload_to='products/')
-    image = models.ImageField(upload_to='products/images/')
+    file = models.FileField(upload_to='products/', null=True, blank=True)
+    image = models.ImageField(upload_to='products/images/', null=True, blank=True)
+    available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']  
 
     def __str__(self):
         return self.name
