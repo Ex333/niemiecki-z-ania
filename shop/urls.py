940a282414a7583977824961c3ef152b7acd76e6
Ex333 <haldas.mateusz@gmail.com>
2026-03-25 20:53:35 +0100

    urls and vies shop and product_detail added

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from shop.views import  sklep
+from shop.views import  sklep, product_detail
 
 
 urlpatterns = [
     path("", sklep, name="sklep"),
+    path("<slug:slug>/", product_detail, name="sklep_detail"),
 ]
 
