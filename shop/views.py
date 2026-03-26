0e45ef16a78151b7eebdf2e63b286c06978955b5
Ex333 <haldas.mateusz@gmail.com>
2026-03-26 12:19:03 +0100

    pagination and css fix - container

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from django.core.paginator import Paginator
+
 
 def sklep(request):
     products = Product.objects.filter(available=True)
-    return render(request, "shop/sklep.html", {"products": products})
+
+    paginator = Paginator(products, 9)  
+    page_number = request.GET.get("page")  
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "shop/sklep.html", {
+        "page_obj": page_obj
+    })
+
 
 def product_detail(request, slug):
-    product = get_object_or_404(Product, slug=slug , available=True)
-    return render(request, "shop/product_detail.html ", {"product": product})
+    product = get_object_or_404(Product, slug=slug, available=True)
+    return render(request, "shop/product_detail.html", {  
+        "product": product
+    })
